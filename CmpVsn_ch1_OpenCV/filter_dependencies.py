ab154a1c8ca74d6f038931615ba93f769031f96d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-07-09 14:16:42 +0600

    Add files via upload

---
+++ CmpVsn_ch1_OpenCV/filter_dependencies.py
@@ -0,0 +1,391 @@
+''' 
+name: virtual_platform
+channels:
+- menpo
+- conda-forge
+- peterjc123
+- defaults
+dependencies:
+- ffmpeg=3.2.4=1
+- freetype=2.7=vc14_1
+- imageio=2.2.0=py35_0
+- libtiff=4.0.6=vc14_7
+- olefile=0.44=py35_0
+- pillow=4.2.1=py35_0
+- vc=14=0
+- alabaster=0.7.10=py35_0
+- astroid=1.5.3=py35_0
+- babel=2.5.0=py35_0
+- bleach=1.5.0=py35_0
+- certifi=2016.2.28=py35_0
+- cffi=1.10.0=py35_0
+- chardet=3.0.4=py35_0
+- colorama=0.3.9=py35_0
+- decorator=4.1.2=py35_0
+- docutils=0.14=py35_0
+- entrypoints=0.2.3=py35_0
+- html5lib=0.9999999=py35_0
+- icu=57.1=vc14_0
+- imagesize=0.7.1=py35_0
+- ipykernel=4.6.1=py35_0
+- ipython=6.1.0=py35_0
+- ipython_genutils=0.2.0=py35_0
+- isort=4.2.15=py35_0
+- jedi=0.10.2=py35_2
+- jinja2=2.9.6=py35_0
+- jpeg=9b=vc14_0
+- jsonschema=2.6.0=py35_0
+- jupyter_client=5.1.0=py35_0
+- jupyter_core=4.3.0=py35_0
+- lazy-object-proxy=1.3.1=py35_0
+- libpng=1.6.30=vc14_1
+- markupsafe=1.0=py35_0
+- mistune=0.7.4=py35_0
+- mkl=2017.0.3=0
+- nbconvert=5.2.1=py35_0
+- nbformat=4.4.0=py35_0
+- numpy=1.13.1=py35_0
+- numpydoc=0.7.0=py35_0
+- openssl=1.0.2l=vc14_0
+	- pandocfilters=1.4.2=py35_0
+- path.py=10.3.1=py35_0
+- pickleshare=0.7.4=py35_0
+- pip=9.0.1=py35_1
+- prompt_toolkit=1.0.15=py35_0
+- psutil=5.2.2=py35_0
+- pycodestyle=2.3.1=py35_0
+- pycparser=2.18=py35_0
+- pyflakes=1.6.0=py35_0
+- pygments=2.2.0=py35_0
+- pylint=1.7.2=py35_0
+- pyqt=5.6.0=py35_2
+- python=3.5.4=0
+- python-dateutil=2.6.1=py35_0
+- pytz=2017.2=py35_0
+- pyzmq=16.0.2=py35_0
+- qt=5.6.2=vc14_6
+- qtawesome=0.4.4=py35_0
+- qtconsole=4.3.1=py35_0
+- qtpy=1.3.1=py35_0
+- requests=2.14.2=py35_0
+- rope=0.9.4=py35_1
+- setuptools=36.4.0=py35_1
+- simplegeneric=0.8.1=py35_1
+- singledispatch=3.4.0.3=py35_0
+- sip=4.18=py35_0
+- six=1.10.0=py35_1
+- snowballstemmer=1.2.1=py35_0
+- sphinx=1.6.3=py35_0
+- sphinxcontrib=1.0=py35_0
+- sphinxcontrib-websupport=1.0.1=py35_0
+- spyder=3.2.3=py35_0
+- testpath=0.3.1=py35_0
+- tornado=4.5.2=py35_0
+- traitlets=4.3.2=py35_0
+- vs2015_runtime=14.0.25420=0
+- wcwidth=0.1.7=py35_0
+- wheel=0.29.0=py35_0
+- win_unicode_console=0.5=py35_0
+- wincertstore=0.2=py35_0
+- wrapt=1.10.11=py35_0
+- zlib=1.2.11=vc14_0
+- opencv3=3.1.0=py35_0
+- pytorch=0.1.12=py35_0.1.12cu80
+
+- pip:
+  - ipython-genutils==0.2.0
+  - jupyter-client==5.1.0
+  - jupyter-core==4.3.0
+  - prompt-toolkit==1.0.15
+  - pyyaml==3.12
+  - rope-py3k==0.9.4.post1
+  - torch==0.1.12
+  - torchvision==0.1.9
+  - win-unicode-console==0.5
+
+
+
+conda install 
+
+
+conda install ipython-genutils==0.2.0
+conda install jupyter-client==5.1.0
+conda install jupyter-core==4.3.0
+conda install prompt-toolkit==1.0.15
+conda install pyyaml==3.12
+conda install rope-py3k==0.9.4.post1
+conda install torch==0.1.12
+conda install torchvision==0.1.9
+conda install win-unicode-console==0.5
+'''
+
+
+yml_lists = [
+"ffmpeg=3.2.4=1",
+"freetype=2.7=vc14_1",
+"imageio=2.2.0=py35_0",
+"libtiff=4.0.6=vc14_7",
+"olefile=0.44=py35_0",
+"pillow=4.2.1=py35_0",
+"vc=14=0",
+"alabaster=0.7.10=py35_0",
+"astroid=1.5.3=py35_0",
+"babel=2.5.0=py35_0",
+"bleach=1.5.0=py35_0",
+"certifi=2016.2.28=py35_0",
+"cffi=1.10.0=py35_0",
+"chardet=3.0.4=py35_0",
+"colorama=0.3.9=py35_0",
+"decorator=4.1.2=py35_0",
+"docutils=0.14=py35_0",
+"entrypoints=0.2.3=py35_0",
+"html5lib=0.9999999=py35_0",
+"icu=57.1=vc14_0",
+"imagesize=0.7.1=py35_0",
+"ipykernel=4.6.1=py35_0",
+"ipython=6.1.0=py35_0",
+"ipython_genutils=0.2.0=py35_0",
+"isort=4.2.15=py35_0",
+"jedi=0.10.2=py35_2",
+"jinja2=2.9.6=py35_0",
+"jpeg=9b=vc14_0",
+"jsonschema=2.6.0=py35_0",
+"jupyter_client=5.1.0=py35_0",
+"jupyter_core=4.3.0=py35_0",
+"lazy-object-proxy=1.3.1=py35_0",
+"libpng=1.6.30=vc14_1",
+"markupsafe=1.0=py35_0",
+"mistune=0.7.4=py35_0",
+"mkl=2017.0.3=0",
+"nbconvert=5.2.1=py35_0",
+"nbformat=4.4.0=py35_0",
+"numpy=1.13.1=py35_0",
+"numpydoc=0.7.0=py35_0",
+"openssl=1.0.2l=vc14_0",
+"pandocfilters=1.4.2=py35_0",
+"path.py=10.3.1=py35_0",
+"pickleshare=0.7.4=py35_0",
+"pip=9.0.1=py35_1",
+"prompt_toolkit=1.0.15=py35_0",
+"psutil=5.2.2=py35_0",
+"pycodestyle=2.3.1=py35_0",
+"pycparser=2.18=py35_0",
+"pyflakes=1.6.0=py35_0",
+"pygments=2.2.0=py35_0",
+"pylint=1.7.2=py35_0",
+"pyqt=5.6.0=py35_2",
+"python=3.5.4=0",
+"python-dateutil=2.6.1=py35_0",
+"pytz=2017.2=py35_0",
+"pyzmq=16.0.2=py35_0",
+"qt=5.6.2=vc14_6",
+"qtawesome=0.4.4=py35_0",
+"qtconsole=4.3.1=py35_0",
+"qtpy=1.3.1=py35_0",
+"requests=2.14.2=py35_0",
+"rope=0.9.4=py35_1",
+"setuptools=36.4.0=py35_1",
+"simplegeneric=0.8.1=py35_1",
+"singledispatch=3.4.0.3=py35_0",
+"sip=4.18=py35_0",
+"six=1.10.0=py35_1",
+"snowballstemmer=1.2.1=py35_0",
+"sphinx=1.6.3=py35_0",
+"sphinxcontrib=1.0=py35_0",
+"sphinxcontrib-websupport=1.0.1=py35_0",
+"spyder=3.2.3=py35_0",
+"testpath=0.3.1=py35_0",
+"tornado=4.5.2=py35_0",
+"traitlets=4.3.2=py35_0",
+"vs2015_runtime=14.0.25420=0",
+"wcwidth=0.1.7=py35_0",
+"wheel=0.29.0=py35_0",
+"win_unicode_console=0.5=py35_0",
+"wincertstore=0.2=py35_0",
+"wrapt=1.10.11=py35_0",
+"zlib=1.2.11=vc14_0",
+"opencv3=3.1.0=py35_0",
+"pytorch=0.1.12=py35_0.1.12cu80"
+]
+
+pip_list = [
+"ipython-genutils==0.2.0",
+"jupyter-client==5.1.0",
+"jupyter-core==4.3.0",
+"prompt-toolkit==1.0.15",
+"pyyaml==3.12",
+"rope-py3k==0.9.4.post1",
+"torch==0.1.12",
+"torchvision==0.1.9",
+"win-unicode-console==0.5"
+]
+
+
+
+
+# -----------------    installed in conda env    -----------------
+
+installed_list = [
+"alabaster                 0.7.12             pyhd3eb1b0_0",
+"asn1crypto                1.4.0                      py_0",
+"astroid                   1.6.5                    py35_0",
+"babel                     2.9.0              pyhd3eb1b0_0",
+"bleach                    3.3.1              pyhd3eb1b0_0",
+"ca-certificates           2023.01.10           haa95532_0",
+"certifi                   2020.6.20          pyhd3eb1b0_3",
+"cffi                      1.11.5           py35h74b6da3_1",
+"chardet                   3.0.4                    py35_1",
+"cloudpickle               1.6.0              pyhd3eb1b0_0",
+"colorama                  0.4.4              pyhd3eb1b0_0",
+"cryptography              2.3.1            py35h74b6da3_0",
+"decorator                 5.1.1              pyhd3eb1b0_0",
+"defusedxml                0.7.1              pyhd3eb1b0_0",
+"docutils                  0.14             py35h8ccb97f_0",
+"entrypoints               0.2.3                    py35_2",
+"icu                       58.2                 ha925a31_3",
+"idna                      2.10               pyhd3eb1b0_0",
+"imagesize                 1.3.0              pyhd3eb1b0_0",
+"ipykernel                 4.10.0                   py35_0",
+"ipython                   5.8.0                    py35_0",
+"ipython_genutils          0.2.0              pyhd3eb1b0_1",
+"isort                     4.3.4                    py35_0",
+"jedi                      0.12.1                   py35_0",
+"jinja2                    2.11.3             pyhd3eb1b0_0",
+"jpeg                      9e                   h2bbff1b_0",
+"jsonschema                2.6.0            py35h27d56d3_0",
+"jupyter_client            5.3.3                      py_0",
+"jupyter_core              4.5.0                      py_0",
+"keyring                   13.2.1                   py35_0",
+"lazy-object-proxy         1.3.1            py35hfa6e2cd_2",
+"libpng                    1.6.37               h2a8f88b_0",
+"libsodium                 1.0.16               h9d3ae62_0",
+"markupsafe                1.0              py35hfa6e2cd_1",
+"mccabe                    0.6.1                    py35_1",
+"mistune                   0.8.3            py35hfa6e2cd_1",
+"nbconvert                 5.5.0                      py_0",
+"nbformat                  5.1.3              pyhd3eb1b0_0",
+"numpy                     1.18.5                   pypi_0    pypi",
+"numpydoc                  1.1.0              pyhd3eb1b0_1",
+"openssl                   1.0.2u               he774522_0",
+"packaging                 20.9               pyhd3eb1b0_0",
+"pandoc                    2.12                 haa95532_1",
+"pandocfilters             1.5.0              pyhd3eb1b0_0",
+"parso                     0.8.0                      py_0",
+"pickleshare               0.7.5           pyhd3eb1b0_1003",
+"pip                       10.0.1                   py35_0",
+"prompt_toolkit            1.0.15           py35h89c7cb4_0",
+"protobuf                  3.19.4                   pypi_0    pypi",
+"psutil                    5.4.7            py35hfa6e2cd_0",
+"pycodestyle               2.7.0              pyhd3eb1b0_0",
+"pycparser                 2.20                       py_2",
+"pyflakes                  2.4.0              pyhd3eb1b0_0",
+"pygments                  2.11.2             pyhd3eb1b0_0",
+"pylint                    1.9.2                    py35_0",
+"pyopenssl                 18.0.0                   py35_0",
+"pyparsing                 2.4.7              pyhd3eb1b0_0",
+"pyqt                      5.9.2            py35h6538335_2",
+"pysocks                   1.6.8                    py35_0",
+"python                    3.5.4               h1357f44_23",
+"python-dateutil           2.8.2              pyhd3eb1b0_0",
+"pytz                      2021.3             pyhd3eb1b0_0",
+"pywin32                   223              py35hfa6e2cd_1",
+"pyzmq                     17.1.2           py35hfa6e2cd_0",
+"qt                        5.9.6            vc14h1e9a669_2",
+"qtawesome                 1.0.1                      py_0",
+"qtconsole                 4.7.7                      py_0",
+"qtpy                      1.11.2             pyhd3eb1b0_0",
+"requests                  2.25.1             pyhd3eb1b0_0",
+"rope                      0.22.0             pyhd3eb1b0_0",
+"setuptools                40.2.0                   py35_0",
+"simplegeneric             0.8.1                    py35_2",
+"sip                       4.19.8           py35h6538335_0",
+"six                       1.16.0             pyhd3eb1b0_1",
+"snowballstemmer           2.1.0              pyhd3eb1b0_0",
+"sphinx                    3.5.3              pyhd3eb1b0_0",
+"sphinxcontrib-applehelp   1.0.2              pyhd3eb1b0_0",
+"sphinxcontrib-devhelp     1.0.2              pyhd3eb1b0_0",
+"sphinxcontrib-htmlhelp    1.0.3              pyhd3eb1b0_0",
+"sphinxcontrib-jsmath      1.0.1              pyhd3eb1b0_0",
+"sphinxcontrib-qthelp      1.0.3              pyhd3eb1b0_0",
+"sphinxcontrib-serializinghtml 1.1.5              pyhd3eb1b0_0",
+"spyder                    3.3.1                    py35_1",
+"spyder-kernels            0.2.6                    py35_0",
+"sqlite                    3.40.1               h2bbff1b_0",
+"tensorflow                1.0.0                    pypi_0    pypi",
+"testpath                  0.4.4              pyhd3eb1b0_0",
+"tornado                   5.1.1            py35hfa6e2cd_0",
+"traitlets                 4.3.2            py35h09b975b_0",
+"urllib3                   1.23                     py35_0",
+"vc                        14.2                 h21ff451_1",
+"vs2015_runtime            14.27.29016          h5e58377_2",
+"wcwidth                   0.2.5              pyhd3eb1b0_0",
+"webencodings              0.5.1                    py35_1",
+"wheel                     0.37.1             pyhd3eb1b0_0",
+"win_inet_pton             1.0.1                    py35_1",
+"win_unicode_console       0.5              py35h56988b5_0",
+"wincertstore              0.2              py35hfebbdb8_0",
+"wrapt                     1.10.11          py35hfa6e2cd_2",
+"zeromq                    4.2.5                he025d50_1",
+"zlib                      1.2.13               h8cc25b3_0"
+]
+
+
+pip_pack = [strs.split('==')[0] for strs in pip_list]
+pip_pack_dict = {strs.split('==')[0] : strs.split('==')[1]  for strs in pip_list}
+yml_dict = {strs.split('=')[0] : strs.split('=')[1]  for strs in yml_lists}
+
+
+installed_list_list = [strs.split(' ')  for strs in installed_list]
+no_spc_installed_list_list = []
+
+# to remove duplicates from list 
+for ls in installed_list_list:
+    # print(ls)
+    ls_1 = [x for x in ls if len(str(x))>1]
+    print(ls_1)
+    no_spc_installed_list_list.append(ls_1)
+
+# printing list after removal 
+print(no_spc_installed_list_list)
+installed_list_names = [ls[0] for ls in no_spc_installed_list_list]
+
+# filter the "Dependencies"
+need_to_install = []
+for key in yml_dict.keys():
+    if key not in installed_list_names:
+        need_to_install.append([key, yml_dict[key]])
+
+for key in pip_pack_dict.keys():
+    if key not in installed_list_names:
+        need_to_install.append([key, pip_pack_dict[key]])
+
+for need_instl in need_to_install:
+    print(f"conda install {need_instl[0]}=={need_instl[1]}")
+
+''' 
+conda install ffmpeg==3.2.4
+conda install freetype==2.7
+conda install imageio==2.2.0
+conda install libtiff==4.0.6
+conda install olefile==0.44
+conda install pillow==4.2.1
+conda install html5lib==0.9999999
+conda install mkl==2017.0.3
+conda install path.py==10.3.1
+conda install singledispatch==3.4.0.3
+conda install sphinxcontrib==1.0
+conda install sphinxcontrib-websupport==1.0.1
+conda install opencv3==3.1.0
+conda install pytorch==0.1.12
+conda install ipython-genutils==0.2.0
+conda install jupyter-client==5.1.0
+conda install jupyter-core==4.3.0
+conda install prompt-toolkit==1.0.15
+conda install pyyaml==3.12
+conda install rope-py3k==0.9.4.post1
+conda install torch==0.1.12
+conda install torchvision==0.1.9
+conda install win-unicode-console==0.5
+'''
+
+# python filter_dependencies.py
